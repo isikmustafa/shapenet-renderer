6a87922f8961e8c1494f78e4d56d16e7c6d4b8cd
Mustafa Işık <mustafaisikmetu@gmail.com>
2019-11-12 15:03:29 +0100

    code duplication removed

--- project/model.h
+++ project/model.h
@@ -48,56 +48,35 @@ public:
 		{
 			auto node = traversal_stack[traversal_stack_top--];
 
-			result = node->left_bbox.intersect(ray_origin, ray_inv_dir);
-			if (result > 0.0f && result < min_distance)
+			for (int child_index = 0; child_index <= 1; ++child_index)
 			{
-				//inner node
-				if (nodes[node->left_node].left_node)
+				result = reinterpret_cast<Bbox*>(node)[child_index].intersect(ray_origin, ray_inv_dir);
+				if (result > 0.0f && result < min_distance)
 				{
-					traversal_stack[++traversal_stack_top] = &nodes[node->left_node];
-				}
-				//leaf node
-				else
-				{
-					if (intersected_ptr)
+					//inner node
+					constexpr int stride = 2 * sizeof(Bbox) / sizeof(unsigned int);
+					auto child_node = reinterpret_cast<unsigned int*>(node)[stride + child_index];
+					if (nodes[child_node].left_node)
 					{
-						for (int i = intersected_ptr->start_index; i <= intersected_ptr->end_index; ++i)
+						traversal_stack[++traversal_stack_top] = &nodes[child_node];
+					}
+					//leaf node
+					else
+					{
+						if (intersected_ptr)
 						{
-							auto result = triangles[i].intersectShadowRay(ray);
-							if (result > 0.0f && result < min_distance)
+							for (int i = intersected_ptr->start_index; i <= intersected_ptr->end_index; ++i)
 							{
-								min_distance = result;
-								triangle_index = i;
+								auto result = triangles[i].intersectShadowRay(ray);
+								if (result > 0.0f && result < min_distance)
+								{
+									min_distance = result;
+									triangle_index = i;
+								}
 							}
 						}
+						intersected_ptr = &nodes[child_node];
 					}
-					intersected_ptr = &nodes[node->left_node];
-				}
-			}
-			result = node->right_bbox.intersect(ray_origin, ray_inv_dir);
-			if (result > 0.0f && result < min_distance)
-			{
-				//inner node
-				if (nodes[node->right_node].right_node)
-				{
-					traversal_stack[++traversal_stack_top] = &nodes[node->right_node];
-				}
-				//leaf node
-				else
-				{
-					if (intersected_ptr)
-					{
-						for (int i = intersected_ptr->start_index; i <= intersected_ptr->end_index; ++i)
-						{
-							auto result = triangles[i].intersectShadowRay(ray);
-							if (result > 0.0f && result < min_distance)
-							{
-								min_distance = result;
-								triangle_index = i;
-							}
-						}
-					}
-					intersected_ptr = &nodes[node->right_node];
 				}
 			}
 
